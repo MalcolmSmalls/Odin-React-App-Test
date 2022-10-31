0fdad3f8f6cfc6252e63336fdafa1f5bb2cf515d
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-31 12:50:27 -0700

    Try functional components

--- src/App.js
+++ src/App.js
@@ -34,43 +34,57 @@
 
 
 
-import React, { Component} from 'react'
+// import React, { Component} from 'react'
 
-class App extends Component {
-	constructor(){
-		super()
-
-
-
-
-    this.state = {
-      count: 0,
-    }
-
-    this.countUp = this.countUp.bind(this)
-	}
+// class App extends Component {
+// 	constructor(){
+// 		super()
 
 
 
 
-	countUp(){
-    const {count} = this.state
-		this.setState({
-			count: this.state.count + 1
-		})	
-	}
+//     this.state = {
+//       count: 0,
+//     }
 
-	render(){
-    const {count} = this.state
-		return (
-			<div>
-				<button onClick={this.countUp}>Click Me!</button>
-				<p>{count}</p>
-			</div>
+//     this.countUp = this.countUp.bind(this)
+// 	}
 
-		)
-	}
 
+
+
+// 	countUp(){
+//     const {count} = this.state
+// 		this.setState({
+// 			count: this.state.count + 1
+// 		})	
+// 	}
+
+// 	render(){
+//     const {count} = this.state
+// 		return (
+// 			<div>
+// 				<button onClick={this.countUp}>Click Me!</button>
+// 				<p>{count}</p>
+// 			</div>
+
+// 		)
+// 	}
+
+// }
+
+
+// export default App
+
+import React, { Component } from 'react'
+import MyComponent from "./MyComponent"
+
+function App(){
+  return (
+    <div>
+      <MyComponent title="UP" />
+    </div>
+  )
 }
 
 
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,23 +1,36 @@
+// import React, { Component } from 'react'
+
+// class MyComponent extends Component {
+//     constructor(props) {
+//         super(props)
+//     }
+
+
+
+//     render(){
+//         const { title, onButtonClicked} = this.props
+        
+//         return (
+//             <div>
+//                 <h1>{title}</h1>
+//                 <button onClick={onButtonClicked}>Click Me</button>
+//             </div>
+//         )
+//     }
+// }
+
+
+// export default MyComponent
+
+
+
 import React, { Component } from 'react'
 
-class MyComponent extends Component {
-    constructor(props) {
-        super(props)
-    }
 
-
-
-    render(){
-        const { title, onButtonClicked} = this.props
-        
-        return (
-            <div>
-                <h1>{title}</h1>
-                <button onClick={onButtonClicked}>Click Me</button>
-            </div>
-        )
-    }
+function MyComponent(props){
+    return (
+      <div>{props.title}</div>
+    )
 }
-
 
 export default MyComponent
