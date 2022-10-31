017abb2fbaa065f287d9f5891a8c90cf4f9e4869
MalcolmSmalls <smallsbeats@gmail.com>
2022-10-31 11:42:21 -0700

    Experiment with State in React

--- src/App.js
+++ src/App.js
@@ -1,32 +1,73 @@
-import './App.css';
-import React, { Component } from 'react';
-import MyComponent from './MyComponent'
+// import './App.css';
+// import React, { Component } from 'react';
+// import MyComponent from './MyComponent'
 
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-  }
+// class App extends Component {
+//   constructor() {
+//     super()
+//   }
 
-  onClickBtn() {
-    this.onClickBtn = this.onClickBtn.bind(this)
-  }
+//   onClickBtn() {
+//     this.onClickBtn = this.onClickBtn.bind(this)
+//   }
 
-  onClickBtn() {
-    console.log('The button was clicked')
-  }
+//   onClickBtn() {
+//     console.log('The button was clicked')
+//   }
   
 
 
-  render(){
-    return (
-      <div>
-        <MyComponent title="React" onButtonClicked={this.onClickBtn}></MyComponent>
-      </div>
-    )
-  }
+//   render(){
+//     return (
+//       <div>
+//         <MyComponent title="React" onButtonClicked={this.onClickBtn}></MyComponent>
+//       </div>
+//     )
+//   }
+
+// }
+
+
+// export default App;
+
+
+
+
+import React, { Component} from 'react'
+
+class App extends Component {
+	constructor(){
+		super()
+
+
+    this.state = {
+      count: 0,
+    }
+
+    this.countUp = this.countUp.bind(this)
+	}
+
+
+
+
+	countUp(){
+		this.setState({
+			count: this.state.count + 1
+		})	
+	}
+
+	render(){
+		return (
+			<div>
+				<button onClick={this.countUp}>Click Me!</button>
+				<p>{this.state.count}</p>
+			</div>
+
+		)
+	}
 
 }
 
 
-export default App;
+export default App
