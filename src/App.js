5c9a1759b61686135c5c0bf05f07341704d7b292
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-01 19:12:24 -0700

    Practice react states

--- src/App.js
+++ src/App.js
@@ -99,36 +99,147 @@
 
 
 
-import MyComponent from "./MyComponent"
-import React, { Component } from "react"
+// import MyComponent from "./MyComponent"
+// import React, { Component } from "react"
 
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      count: 0,
-    }
-    this.countUp = this.countUp.bind(this)
-  }
+// class App extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       count: 0,
+//     }
+//     this.countUp = this.countUp.bind(this)
+//   }
 
-  countUp(){
-    const {count} = this.state 
-      this.setState ({
-        count: this.state.count + 1
-      })
-    }
+//   countUp(){
+//     const {count} = this.state 
+//       this.setState ({
+//         count: this.state.count + 1
+//       })
+//     }
   
 
-  render(){
-    const {count} = this.state
-    return (
-      <div>
-        <p><button onClick={this.countUp}>Click</button></p>
-        <p>{count}</p>
-      </div>
-    )
-  }
+//   render(){
+//     const {count} = this.state
+//     return (
+//       <div>
+//         <p><button onClick={this.countUp}>Click</button></p>
+//         <p>{count}</p>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
+
+
+
+
+
+// import React, { Component } from "react"
+
+// class App extends Component {
+//   constructor(){
+//     super()
+    
+//     this.state = {
+//       count:0,
+//     }
+
+//     this.countUp = this.countUp.bind(this)
+//   }
+
+//   countUp(){
+//     const { count } = this.state
+//     this.setState ({
+//       count: this.state.count + 1
+//     })
+//   }
+  
+//   render(){
+//     return (
+//       <div>
+//         <button onClick={this.countUp}>Click Here</button>
+//         <p>{this.state.count}</p>
+//       </div>
+//     )
+//   }
+
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+import React, { Component } from "react"
+
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
+	
+	componentDidMount() {
+		this.timerID = setInterval(
+			() =>  this.tick(),
+			1000
+		)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID)
+	}
+
+	tick(){
+		this.setState({
+			date: new Date()
+		})
+	}
+
+	render(){
+		return (
+			<div>
+				<h1>Hello, world!</h1>
+				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+			</div>
+			)
+	}
 }
 
-export default App
+// class Clock extends Component {
+//   constructor(props){
+//     super(props)
+
+//     this.state = {
+//       count:0,
+//     }
+
+//     this.countUp = this.countUp.bind(this)
+//   }
+
+//   countUp(){
+//     const { count } = this.state
+//     this.setState ({
+//       count: this.state.count + 1
+//     })
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <p><button onClick={this.countUp}></button></p>
+//         <p>{this.state.count}</p>
+//       </div>
+//     )
+//   }
+
+// }
+
+export default Clock
